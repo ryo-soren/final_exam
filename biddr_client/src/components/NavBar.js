4bab16d15c846ba22fe15475dcb79f8b10e5d2bf
ryoS <ryosorensen0606@gmail.com>
2022-12-07 22:17:43 -0800

    added name feature

--- biddr_client/src/components/NavBar.js
+++ biddr_client/src/components/NavBar.js
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 import { Session } from "../requests";
 
 const NavBar = (props) => {
-
-    const {currentUser, onSignOut} = props
+    
+    const {currentUser, onSignOut, name} = props
 
     const signOut = () => {
         Session.destroy().then(() => {
@@ -21,6 +21,8 @@ const NavBar = (props) => {
                     currentUser?(
                         <>
                         <NavLink to='/auctions/new'>New Auction</NavLink>
+                        <span>|</span>
+                        <span>Hello {name}</span>
                         <span>|</span>
                         <button onClick={signOut}>Sign Out</button>
                         </>
